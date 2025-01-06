a8bdd60b3582fb756e49edea5ce7fa14a1c8156d
MyatCharm <myintmyatzaw@gmail.com>
2025-01-06 15:58:37 +0700

    adding type annotations to mean_and_median.py

--- solutions/mean_and_median.py
+++ solutions/mean_and_median.py
@@ -1,21 +1,25 @@
-def mean_and_median(values):
+from typing import List, Tuple
+
+
+def mean_and_median(values: List[float]) -> Tuple[float, float]:
     """
     Functions:
       Get the mean and median of a sorted list of `values`
 
     Args:
-      values (iterable of float): A list of numbers
+      values (List[float]): A list of numbers
 
     Returns:
-      tuple (float, float): The mean and median
+      Tuple[float, float]: The mean and median
 
     Raises:
-          TypeError: If `values` is not a list.
+      TypeError: If `values` is not a list.
     """
     if not isinstance(values, list):
         raise TypeError("Input must be a list.")
     if len(values) == 0:
-        return None, None
+        raise ValueError("Input list must not be empty.")
+
     mean = sum(values) / len(values)
     values = sorted(values)
     midpoint = int(len(values) / 2)
